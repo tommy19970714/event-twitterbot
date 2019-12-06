c2104944fb9a1ecb606e8a02c5b2912141a0ccf6
toshiki tomihira <toshiki19970714@gmail.com>
2019-12-07 01:25:01 +0900

    use /participation endpoint

--- src/eventpage.py
+++ src/eventpage.py
@@ -3,6 +3,7 @@
 from connpass import Connpass
 from bs4 import BeautifulSoup
 import requests
+import urllib.parse
 
 class Event():
     def __init__(self):
@@ -13,7 +14,6 @@ class Event():
         response = self.connpass.search(ymd=date_array, count=count, start=self.start)
         self.start += response["results_returned"]
         return response["events"]
-        
 
     def from_ids(self, event_ids, count=1):
         response = self.connpass.search(event_id=event_ids, count=count, start=self.start)
@@ -26,41 +26,51 @@ class Page():
         r = requests.get(url)
         self.soup = BeautifulSoup(r.text, "html.parser")
 
+    def twitter_filter(self, domain):
+        return 'twitter' in domain if True else False
+
+    def extract_username(self, url):
+        qs = urllib.parse.urlparse(url).query
+        qs_d = urllib.parse.parse_qs(qs)
+        return qs_d["screen_name"][0] if "screen_name" in qs_d.keys() else None
+
+    def tag2url(self, tags):
+        urls = map(lambda t: self.extract_username(t.get("href")), tags)
+        return list(filter(lambda x: x != None, urls))
+
     def organizers(self):
-        tags = self.soup.select(".group_inner.event_owner_area > ul > li > a")
-        return list(map(lambda t: t.get("href"), tags))
+        tags = self.soup.select(".concerned_area > table > tbody")[
+            0].select("td.social.text_center > a")
+        return self.tag2url(tags)
 
     def presenters(self):
-        tags = self.soup.select(".event_presenters_area.clearfix > a")
-        return list(map(lambda t: t.get("href"), tags))
-
-
-class Profile():
-    def __init__(self, url):
-        r = requests.get(url)
-        self.soup = BeautifulSoup(r.text, "html.parser")
+        tags = self.soup.select(".concerned_area > table > tbody")[
+            1].select("td.social.text_center > a")
+        return self.tag2url(tags)
     
-    def twitter(self):
-        tag = self.soup.select(".social_link > a")[0]
-        return tag.get("href")
+    def attendees(self):
+        tags = self.soup.select(".participation_table_area > table > tbody > tr > td.social.text_center > a")
+        return self.tag2url(tags)
 
+
+def participation_url(event):
+    return event["event_url"] + "participation"
 
 if __name__ == "__main__":
+
     events = Event().from_ids([151291])
     for event in events:
-        url = event["event_url"]
+        url = participation_url(event)
         page = Page(url)
+
+        print("-- organizers --")
+        organizers = page.organizers()
+        print(organizers)
 
         print("-- presenters --")
         presenters = page.presenters()
-        for url in presenters:
-            profile = Profile(url)
-            print(profile.twitter())
-        
-        print("-- organizers --")
-        organizers = page.organizers()
-        for url in organizers:
-            profile = Profile(url)
-            print(profile.twitter())
+        print(presenters)
 
-
+        print("-- attendees --")
+        attendees = page.attendees()
+        print(attendees)
