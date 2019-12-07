91beb8213a9fbc50b4369e6f10e654c8358c246f
qqhann <qiu.gits@gmail.com>
2019-12-07 12:20:00 +0900

    WIP: concept

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ import tweepy
 import src.eventpage as eventpage
 
 if __name__ == "__main__":
-    pass
+    user = api.get_user(username)
+    db.save_twitter_user(user)
+
+    for user in db.users.ffrate():
+        if user.is_good_to_follow():
+            me.follow(user)
     
