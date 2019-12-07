ba77f7ae33b5e2b005d54f8125b2eb683111a1fe
qqhann <qiu.gits@gmail.com>
2019-12-07 12:24:47 +0900

    Refactor: format

--- src/database.py
+++ src/database.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 class Database():
     def __init__(self):
         self.connection = sqlite3.connect("./twitterbot.sqlite")
@@ -7,13 +8,16 @@ class Database():
         self.cursor = self.connection.cursor()
         self.cursor.execute("DROP TABLE IF EXISTS twitter_users")
         self.cursor.execute(
-            "CREATE TABLE IF NOT EXISTS twitter_users (id INTEGER PRIMARY KEY AUTOINCREMENT, screen_name VARCHAR(64), flag BOOL)")
-    
+            """CREATE TABLE IF NOT EXISTS twitter_users (
+                id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                screen_name VARCHAR(64), 
+                flag BOOL)""")
+
     def add_user(self, screen_name):
         self.cursor.execute(
             'INSERT OR IGNORE INTO twitter_users (screen_name, flag) VALUES (?, ?)', (screen_name, 0))
         self.connection.commit()
-    
+
     def read_users(self):
         self.cursor.execute('SELECT * FROM twitter_users')
         return self.cursor.fetchall()
@@ -22,5 +26,3 @@ class Database():
         self.cursor.execute(
             'UPDATE twitter_users SET flag = ? WHERE id = ?', (flag, id))
         self.connection.commit()
-
-
