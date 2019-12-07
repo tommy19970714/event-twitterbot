d69039edc3b1647e98f2f6ded02e237205699ba4
qqhann <qiu.gits@gmail.com>
2019-12-07 11:37:02 +0900

    Update: split get_api func

--- src/twitter.py
+++ src/twitter.py
@@ -7,13 +7,20 @@ load_dotenv()
 consumer_key = os.getenv("TW_CS_CONSUMERKEY")
 consumer_secret = os.getenv("TW_CS_CONSUMERSECRET")
 access_token = os.getenv("TW_CS_ACCESSTOKEN")
-access_token_secret  = os.getenv("TW_CS_ACCESSTOKENSECRET")
+access_token_secret = os.getenv("TW_CS_ACCESSTOKENSECRET")
 
-if __name__ == '__main__':
+
+def get_api(consumer_key, consumer_secret, access_token, access_token_secret):
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
+    return api
+
+
+if __name__ == '__main__':
+    api = get_api(consumer_key, consumer_secret,
+                  access_token, access_token_secret)
 
     public_tweets = api.home_timeline()
     for tweet in public_tweets:
